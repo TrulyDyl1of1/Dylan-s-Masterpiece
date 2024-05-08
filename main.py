6ed63a80268805d3c817f78fc2b66d88895edb88
TrulyDyl1of1 <168611869+TrulyDyl1of1@users.noreply.github.com>
2024-05-08 11:02:48 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import pygame
 import time
 import math
 
+
+def scale_image(img, factor):
+    size = round(img.get_width() * factor), round(img.get_height() * factor)
+    return pygame.transform.scale(img, size)
+
+
 GRASS = pygame.image.load("imgs/grass.png")
 TRACK = pygame.image.load("imgs/track.png")
 TRACK_BORDER = pygame.image.load("imgs/track-border.png")
@@ -10,7 +16,7 @@ RED_CAR = pygame.image.load("imgs/red-car.png")
 GREEN_CAR = pygame.image.load("imgs/green-car.png")
 
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
-WIDTH = pygame.display.set_mode(WIDTH,HEIGHT)
+WIN = pygame.display.set_mode(WIDTH,HEIGHT)
 pygame.display.set_caption("Racing Game!!")
 
 FPS = 60
@@ -20,6 +26,11 @@ pygame.time.Clock()
 
 while run:
     time.clock.tick(FPS)
+    WIN.blit(GRASS, (0,0))
+    WIN.blit(TRACK, (0,0))
+    WIN.blit(FINISH, (0,0))
+
+    pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
